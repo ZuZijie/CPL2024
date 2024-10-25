0a04ed719aedb62346f722caec41766e2e5069dc
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-10-25 09:44:13 +0800

    2024-4

--- 2024-4/match.c
+++ 2024-4/match.c
@@ -1,32 +1,28 @@
 #include "stdio.h"
-int findTwoIn2DArray(int arr[][502], int rows, int cols, int target, int *firstRow, int *firstCol, int *secondRow, int *secondCol) {
-    int foundCount = 0;
-
+void findAllPairs(int arr[][502], int rows, int cols, int result[][4]) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            if (arr[i][j] == target) {
-                if (foundCount == 0) {
-                    *firstRow = i;
-                    *firstCol = j;
-                    foundCount++;
-                } else if (foundCount == 1) {
-                    *secondRow = i;
-                    *secondCol = j;
-                    foundCount++;
-                    return 1;
-                }
+            int num = arr[i][j];  
+            if (result[num][0] == 0 && result[num][1] == 0) {
+                result[num][0] = i;
+                result[num][1] = j;
+            } else {
+                result[num][2] = i;
+                result[num][3] = j;
             }
         }
     }
-    return 0; 
 }
+
+
 int main()
 {
     int n,m;
     int firstrow,secondrow,firstcol,secondcol;
     scanf("%d %d",&n,&m);
     int arr[502][502]={0};
-    int ans[114514]={0};
+    int ans[125000]={0};
+    int result[125000][4]={0};
     for(int i=1;i<n+1;i++)
     {
         for(int j=1;j<m+1;j++)
@@ -34,9 +30,13 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
+    findAllPairs(arr, n+2, m+2,result);
     for(int k=1;k<((n*m)/2+1);k++)
-    {
-        findTwoIn2DArray(arr,n+2,m+2,k,&firstrow,&firstcol,&secondrow,&secondcol);
+    {   
+        firstrow=result[k][0];
+        firstcol=result[k][1];
+        secondrow=result[k][2];
+        secondcol=result[k][3];
         int temp=1;
         for(int i=firstrow-1;i>=0;i--)
         {
